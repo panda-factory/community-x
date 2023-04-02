8ed29c8955b591ed81e71fa154270b2608a3b469
panda-factory <276715287@qq.com>
2023-04-02 22:32:37 +0800

    [add] 依赖项

--- pages/tabBar/home/home.vue
+++ pages/tabBar/home/home.vue
@@ -1,6 +1,18 @@
 <template>
 	<view>
-		
+		<!-- 02. 轮播区 -->
+		<view v-if="swiperDatas && swiperDatas.length > 0" class="swiper-area margin-lr-sm border-radius pos-r">
+			<!-- 轮播组件 -->
+			<swiper class="swiper w-full" :autoplay="autoplay" indicator-dots indicator-color="#f7f7f7" indicator-active-color="#ff6a6c"
+				previous-margin="30px" next-margin="30px" circular
+				:current="swiperIdx" @change="swiperChange">
+				<swiper-item v-for="(item, idx) in swiperDatas" :key="idx" class="swiper-item wh-full box-sizing-b">
+					<view class="wh-full animated-all" :style="{ 'transform': `scale(${swiperIdx == idx ? 1 : 0.93})` }" @click.stop="topage(item)">
+						<image class="border-radius wh-full" mode="aspectFill" lazy-load :src="item.img" />
+					</view>
+				</swiper-item>
+			</swiper>
+		</view>
 	</view>
 </template>
 
