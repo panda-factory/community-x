c8cec1fbcceabaf2777dddf3ded85bcf48f6f0af
panda-factory <276715287@qq.com>
2023-04-18 00:11:24 +0800

    [feature]: 添加搜索栏

---
+++ pages/tab-bar/forum/components/media-card/media-goods.vue
@@ -0,0 +1,75 @@
+<template>
+    <view class="container">
+        <media-tmpl padding="0" spacing="0">
+            <template v-slot:cover>
+                <view class="custom-cover">
+                    <image class="cover-image" mode="aspectFill" :src="options.imageUrls && options.imageUrls[0]">
+                    </image>
+                    <view class="cover-content">
+                        <text class="uni-subtitle uni-white">{{options.title}}</text>
+                    </view>
+                </view>
+            </template>
+        </media-tmpl>
+    </view>
+</template>
+
+<script>
+    import mediaTmpl from './media-tmpl.vue';
+
+    export default {
+        name: 'MediaCard',
+        components: {
+            mediaTmpl,
+        },
+        props: {
+            options: {
+                type: Object,
+                default: function(e) {
+                    return {
+                        // title:
+                        // imageUrls:
+                        // dateTime: 
+                        // forwardCount: 100, 转发数
+                        // commentCount: 50,  评论数
+                        // likeCount: 200     点赞数
+
+                    }
+                }
+            }
+        },
+        methods: {}
+    }
+</script>
+
+<style lang="scss">
+    .container {
+        overflow: hidden;
+    }
+
+	.custom-cover {
+		flex: 1;
+		flex-direction: row;
+		position: relative;
+	}
+
+	.cover-image {
+		flex: 1;
+		height: 150px;
+	}
+
+	.cover-content {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		height: 40px;
+		background-color: rgba($color: #000000, $alpha: 0.4);
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		padding-left: 15px;
+		font-size: 14px;
+		color: #fff;
+	}
+</style>
--- pages/tab-bar/message/message.vue
+++ pages/tab-bar/message/message.vue
@@ -1,22 +1,114 @@
 <template>
-    <view>
-        
+    <view class="container">
+
+        <uni-search-bar @confirm="search" :focus="true" :placeholder="searchPlacehold" @blur="blur" @focus="focus"
+            @input="input" @cancel="cancel" @clear="clear">
+        </uni-search-bar>
+
+        <view class="tag-view">
+            <uni-tag text="百货" type="primary" />
+            <uni-tag text="家电" type="primary" />
+            <uni-tag text="家具" type="primary" />
+        </view>
+        <view class="row" v-for="(goods,index) in options" :key="index">
+            <view class="col">
+                <media-goods class="left" :options="goods" />
+            </view>
+            <view class="col">
+                <media-goods class="right" :options="goods" />
+            </view>
+        </view>
+
     </view>
 </template>
 
 <script>
+    import mediaGoods from '../forum/components/media-card/media-goods.vue';
+
     export default {
+        components: {
+            mediaGoods
+        },
         data() {
             return {
-                
+                searchPlacehold: '飞机杯',
+                options: [{
+                    title: '测试',
+                    imageUrls: [
+                        'https://mp-d39adc1c-4ce7-4397-833e-f2247d112870.cdn.bspapp.com/cloudstorage/5f6a59dc-993c-4b58-808d-3796044319bd.PNG'
+                    ]
+                }, {
+                    title: '测试',
+                    imageUrls: [
+                        'https://mp-d39adc1c-4ce7-4397-833e-f2247d112870.cdn.bspapp.com/cloudstorage/5f6a59dc-993c-4b58-808d-3796044319bd.PNG'
+                    ]
+                }, {
+                    title: '测试',
+                    imageUrls: [
+                        'https://mp-d39adc1c-4ce7-4397-833e-f2247d112870.cdn.bspapp.com/cloudstorage/5f6a59dc-993c-4b58-808d-3796044319bd.PNG'
+                    ]
+                }, {
+                    title: '测试',
+                    imageUrls: [
+                        'https://mp-d39adc1c-4ce7-4397-833e-f2247d112870.cdn.bspapp.com/cloudstorage/5f6a59dc-993c-4b58-808d-3796044319bd.PNG'
+                    ]
+                }, {
+                    title: '测试',
+                    imageUrls: [
+                        'https://mp-d39adc1c-4ce7-4397-833e-f2247d112870.cdn.bspapp.com/cloudstorage/5f6a59dc-993c-4b58-808d-3796044319bd.PNG'
+                    ]
+                }, {
+                    title: '测试',
+                    imageUrls: [
+                        'https://mp-d39adc1c-4ce7-4397-833e-f2247d112870.cdn.bspapp.com/cloudstorage/5f6a59dc-993c-4b58-808d-3796044319bd.PNG'
+                    ]
+                }, {
+                    title: '测试',
+                    imageUrls: [
+                        'https://mp-d39adc1c-4ce7-4397-833e-f2247d112870.cdn.bspapp.com/cloudstorage/5f6a59dc-993c-4b58-808d-3796044319bd.PNG'
+                    ]
+                }, {
+                    title: '测试',
+                    imageUrls: [
+                        'https://mp-d39adc1c-4ce7-4397-833e-f2247d112870.cdn.bspapp.com/cloudstorage/5f6a59dc-993c-4b58-808d-3796044319bd.PNG'
+                    ]
+                }]
             }
         },
         methods: {
-            
+
         }
     }
 </script>
 
-<style>
+<style lang="scss">
+    .container {
+        overflow: hidden;
 
+        .tag-view {
+            margin-right: 10px;
+        }
+
+        .row {
+            display: flex;
+            flex-wrap: wrap;
+            margin: -10rpx;
+            /* 为了抵消uni-card中的padding，这里设置为-10rpx */
+
+            .col {
+                width: 50%;
+                // padding: 10rpx;
+                /* 为了与上面的margin相抵消，这里设置为10rpx */
+                box-sizing: border-box;
+            }
+
+            .left {
+                // padding-right: 5px;
+            }
+
+            .right {
+                // padding-left: 5px;
+            }
+        }
+    }
 </style>
