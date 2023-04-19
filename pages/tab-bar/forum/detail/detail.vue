78b73511351e6d8d850d987c261c8e8ef5cfb054
panda-factory <276715287@qq.com>
2023-04-19 21:49:16 +0800

    [feature]: 详情页添加输入框

--- pages/tab-bar/forum/detail/detail.vue
+++ pages/tab-bar/forum/detail/detail.vue
@@ -1,5 +1,5 @@
 <template>
-    <view>
+    <view class="container">
         <!-- 标题 -->
         <view>
             <text>{{title}}</text>
@@ -12,7 +12,11 @@
                 </swiper-item>
             </swiper>
         </uni-swiper-dot>
-<comment-list></comment-list>
+        <comment-list></comment-list>
+
+        <view class="input-box">
+            <uni-easyinput suffixIcon="paperplane" v-model="commentInput" placeholder="评论" @iconClick="sendComment"/>
+        </view>
     </view>
 </template>
 
@@ -24,21 +28,55 @@
         },
         data() {
             return {
+                bannerId: '',
                 imageUrls: [],
-                title: ''
+                title: '',
+                commentInput: ''
             }
         },
         onLoad(options) {
             console.log('gzx detail onLoad: ' + options.data);
             let data = JSON.parse(options.data);
+            this.bannerId = data._id;
             this.imageUrls = data.imageUrls;
-            this.title = data.title
+            this.title = data.title;
         },
         methods: {
+            sendComment: function() {
+                let commentBanner = {
+                    bannerId: this.bannerId,
+                    userId: getApp().globalData.userInfo._id,
+                    value: this.commentInput
+                }
+
+                console.log('gzx sendComment: ' + JSON.stringify(commentBanner))
+            }
         }
     }
 </script>
 
-<style>
+<style lang="scss">
+    .container {
+        display: flex;
+        flex-direction: column;
+        // height: 100vh;
+    }
 
+    .input-box {
+        display: flex;
+        height: 60px;
+        align-items: center;
+        padding: 10px;
+        background-color: #fff;
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+
+        .input {
+            flex: 1;
+            height: 40px;
+            margin-right: 10px;
+            padding: 0 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+    }
 </style>
