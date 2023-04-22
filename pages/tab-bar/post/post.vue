5ccb6ea57d8a0f5e12f383e608806d22d5559da4
panda-factory <276715287@qq.com>
2023-04-22 11:54:19 +0800

    [feautre]: 发帖页面更新

--- pages/tab-bar/post/post.vue
+++ pages/tab-bar/post/post.vue
@@ -1,13 +1,38 @@
 <template>
-    <view>
-        <uni-file-picker v-model="imageUrls" ref="selectedFiles" :auto-upload="false" limit="9"
-            title="最多选择9张图片"></uni-file-picker>
-        <view>
-            <uni-easyinput v-model="title" placeholder="填写标题" :placeholderStyle="placeholderStyle"></uni-easyinput>
-        </view>
-        <uni-easyinput v-model="description" placeholder="说说此刻的想法"></uni-easyinput>
+    <view class="cx-container">
+        <uni-notice-bar show-icon scrollable text="提醒：请勿发布广告、涉政、涉黄、涉暴以及侵犯他人合法权益的言论。共同遵守社区文明公约，讨论事情对事不对人，不欢迎人身攻击。"/>
 
-        <button type="primary" @click="submit">发布</button>
+        <view class="text-content">
+            <uni-easyinput v-model="title" :inputBorder="false" placeholder="填写标题"
+                :placeholderStyle="placeholderStyle"></uni-easyinput>
+            <uni-easyinput v-model="description" :inputBorder="false" placeholder="和大家分享点什么......"></uni-easyinput>
+
+        </view>
+        <uni-file-picker v-model="imageUrls" ref="selectedFiles" :auto-upload="false" limit="9"></uni-file-picker>
+
+        <view class="option-container">
+            <uni-list>
+                <uni-list-item title="所在位置" show-arrow>
+                    <template v-slot:header>
+                        <uni-icons type="location"></uni-icons>
+                    </template>
+                </uni-list-item>
+                <uni-list-item title="游客不可见">
+
+                    <template v-slot:header>
+                        <uni-icons type="auth"></uni-icons>
+                    </template>
+                    <template v-slot:footer>
+                        <switch checked style="height: 30rpx;transform:scale(0.6) ;"/>
+                    </template>
+                </uni-list-item>
+            </uni-list>
+
+        </view>
+
+        <view class="bottom-bt">
+            <button type="primary" style="width: 60%;" @click="submit">发布帖子</button>
+        </view>
     </view>
 </template>
 
@@ -28,6 +53,7 @@
                     cloudPath: ''
                 },
                 placeholderStyle: "font-weight: bold;font-size:20px",
+
             }
         },
         methods: {
@@ -47,7 +73,7 @@
 
 
                 await this.$refs.selectedFiles.upload();
-                
+
                 console.log('gzx submit 2: ' + this.imageUrls.length)
                 console.log('gzx submit 3: ' + JSON.stringify(this.imageUrls))
                 this.imageUrls.forEach(element => {
@@ -62,9 +88,25 @@
     }
 </script>
 
-<style>
-    .wrapper {
-        font-size: 20px;
-        background-color: #FFFFFF;
+<style lang="scss">
+    .text-content {
+        margin-bottom: 50rpx;
+    }
+
+    .option-container {
+        margin-top: 100rpx;
+    }
+
+    .bottom-bt {
+        display: flex;
+        align-items: center;
+        padding: 15rpx;
+        flex-direction: column;
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        box-shadow: 16px 16px 32px grey;
+        background-color: #ffffff;
     }
 </style>
